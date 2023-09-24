eb030b3500d9a4b0bc5393951e34443a8460fcbc
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-09-23 20:22:01 -0400

    defines map function

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titleCasedTutorials = tutorials.map(tutorial => {
+    const splittedTutorial = tutorial.split(' ')
+    const splittedTitleTutorial = splittedTutorial.map(title => {
+      return title.charAt(0).toUpperCase() + title.slice(1)
+    })
+     return splittedTitleTutorial.join(' ')
+  })
+  return titleCasedTutorials 
 }
